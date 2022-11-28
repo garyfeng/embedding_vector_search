7dadfaf67718bf30a9e4741a6d31ca58ea80235a
Gary Feng <gary.feng@gmail.com>
2022-11-28 08:38:36 -0500

    #6, milvus bulk upload expt; not working

--- data/bulkupload.py
+++ data/bulkupload.py
@@ -1,6 +1,82 @@
-from pymilvus import utility
-task_id = utility.do_bulk_insert(
-    collection_name="book",
-    partition_name="2022",
-    files=["books.json"]
+from pymilvus import (
+    connections,
+    utility,
+    FieldSchema,
+    CollectionSchema,
+    DataType,
+    Collection,
 )
+
+# Connects to a server:
+milvus_host = "host.docker.internal"
+connections.connect("default", host=milvus_host, port="19530")
+
+# Creates a collection:
+fields = [
+    FieldSchema(name="book_id", dtype=DataType.INT64, is_primary=True, auto_id=False),
+    FieldSchema(name="word_count", dtype=DataType.INT64),
+    FieldSchema(name="book_intro", dtype=DataType.FLOAT_VECTOR, dim=2)
+]
+schema = CollectionSchema(fields, "sample books")
+book_collection_name = "book2"
+book_collection = Collection(book_collection_name, schema)
+
+task_id = utility.do_bulk_insert(
+    collection_name=book_collection_name,
+    # partition_name="2022",
+    files=["/data/books.json"]
+)
+
+index = {
+    "index_type": "IVF_FLAT",
+    "metric_type": "L2",
+    "params": {"nlist": 128},
+}
+book_collection.create_index("book_intro", index)
+
+book_collection.load()
+
+
+# #Inserts vectors in the collection:
+# import random
+# entities = [
+#     [i for i in range(3000)],  # field pk
+#     [float(random.randrange(-20, -10)) for _ in range(3000)],  # field random
+#     [[random.random() for _ in range(8)] for _ in range(3000)],  # field embeddings
+# ]
+# insert_result = hello_milvus.insert(entities)
+
+# # Builds indexes on the entities:
+# index = {
+#     "index_type": "IVF_FLAT",
+#     "metric_type": "L2",
+#     "params": {"nlist": 128},
+# }
+# hello_milvus.create_index("embeddings", index)
+
+# # Loads the collection to memory and performs a vector similarity search:
+# hello_milvus.load()
+# vectors_to_search = entities[-1][-2:]
+# search_params = {
+#     "metric_type": "L2",
+#     "params": {"nprobe": 10},
+# }
+# result = hello_milvus.search(vectors_to_search, "embeddings", search_params, limit=3, output_fields=["random"])
+
+# # Performs a vector query:
+# result = hello_milvus.query(expr="random > -14", output_fields=["random", "embeddings"])
+
+# #Performs a hybrid search:
+# result = hello_milvus.search(vectors_to_search, "embeddings", search_params, limit=3, expr="random > -12", output_fields=["random"])
+
+# # Deletes entities by their primary keys:
+# expr = f"pk in [{ids[0]}, {ids[1]}]"
+# hello_milvus.delete(expr)
+
+# # Drops the collection:
+# utility.drop_collection("hello_milvus")
+
+
+
+
+
--- data/hello_milvus.py
+++ data/hello_milvus.py
@@ -1,173 +1,124 @@
-# hello_milvus.py demonstrates the basic operations of PyMilvus, a Python SDK of Milvus.
-# 1. connect to Milvus
-# 2. create collection
-# 3. insert data
-# 4. create index
-# 5. search, query, and hybrid search on entities
-# 6. delete entities by PK
-# 7. drop collection
+# Copyright (C) 2019-2020 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing permissions and limitations under the License.
+
 
 import random
+import numpy as np
 import time
-
+import argparse
 from pymilvus import (
-    connections,
-    utility,
+    connections, list_collections,
     FieldSchema, CollectionSchema, DataType,
-    Collection,
+    Collection
 )
-
-milvus_host = "host.docker.internal"
-
-fmt = "\n=== {:30} ===\n"
-search_latency_fmt = "search latency = {:.4f}s"
-
-#################################################################################
-# 1. connect to Milvus
-# Add a new connection alias `default` for Milvus server in `localhost:19530`
-# Actually the "default" alias is a buildin in PyMilvus.
-# If the address of Milvus is the same as `localhost:19530`, you can omit all
-# parameters and call the method as: `connections.connect()`.
-#
-# Note: the `using` parameter of the following methods is default to "default".
-print(fmt.format("start connecting to Milvus"))
-connections.connect("default", host=milvus_host, port="19530")
-
-has = utility.has_collection("hello_milvus")
-print(f"Does collection hello_milvus exist in Milvus: {has}")
-
-#################################################################################
-# 2. create collection
-# We're going to create a collection with 3 fields.
-# +-+------------+------------+------------------+------------------------------+
-# | | field name | field type | other attributes |       field description      |
-# +-+------------+------------+------------------+------------------------------+
-# |1|    "pk"    |    Int64   |  is_primary=True |      "primary field"         |
-# | |            |            |   auto_id=False  |                              |
-# +-+------------+------------+------------------+------------------------------+
-# |2|  "random"  |    Double  |                  |      "a double field"        |
-# +-+------------+------------+------------------+------------------------------+
-# |3|"embeddings"| FloatVector|     dim=8        |  "float vector with dim 8"   |
-# +-+------------+------------+------------------+------------------------------+
-fields = [
-    FieldSchema(name="pk", dtype=DataType.INT64, is_primary=True, auto_id=False),
-    FieldSchema(name="random", dtype=DataType.DOUBLE),
-    FieldSchema(name="embeddings", dtype=DataType.FLOAT_VECTOR, dim=8)
-]
-
-schema = CollectionSchema(fields, "hello_milvus is the simplest demo to introduce the APIs")
-
-print(fmt.format("Create collection `hello_milvus`"))
-hello_milvus = Collection("hello_milvus", schema, consistency_level="Strong")
-
-################################################################################
-# 3. insert data
-# We are going to insert 3000 rows of data into `hello_milvus`
-# Data to be inserted must be organized in fields.
-#
-# The insert() method returns:
-# - either automatically generated primary keys by Milvus if auto_id=True in the schema;
-# - or the existing primary key field from the entities if auto_id=False in the schema.
-print(fmt.format("Start inserting entities"))
-num_entities = 3000
-entities = [
-    # provide the pk field because `auto_id` is set to False
-    [i for i in range(num_entities)],
-    [float(random.randrange(-20, -10)) for _ in range(num_entities)],  # field random
-    [[random.random() for _ in range(8)] for _ in range(num_entities)],  # field embeddings
-]
-
-insert_result = hello_milvus.insert(entities)
-
-print(f"Number of entities in Milvus: {hello_milvus.num_entities}")  # check the num_entites
-
-################################################################################
-# 4. create index
-# We are going to create an IVF_FLAT index for hello_milvus collection.
-# create_index() can only be applied to `FloatVector` and `BinaryVector` fields.
-print(fmt.format("Start Creating index IVF_FLAT"))
-index = {
-    "index_type": "IVF_FLAT",
-    "metric_type": "L2",
-    "params": {"nlist": 128},
-}
-
-hello_milvus.create_index("embeddings", index)
-
-################################################################################
-# 5. search, query, and hybrid search
-# After data were inserted into Milvus and indexed, you can perform:
-# - search based on vector similarity
-# - query based on scalar filtering(boolean, int, etc.)
-# - hybrid search based on vector similarity and scalar filtering.
-#
-
-# Before conducting a search or a query, you need to load the data in `hello_milvus` into memory.
-print(fmt.format("Start loading"))
-hello_milvus.load()
-
-# -----------------------------------------------------------------------------
-# search based on vector similarity
-print(fmt.format("Start searching based on vector similarity"))
-vectors_to_search = entities[-1][-2:]
-search_params = {
-    "metric_type": "l2",
-    "params": {"nprobe": 10},
-}
-
-start_time = time.time()
-result = hello_milvus.search(vectors_to_search, "embeddings", search_params, limit=3, output_fields=["random"])
-end_time = time.time()
-
-for hits in result:
-    for hit in hits:
-        print(f"hit: {hit}, random field: {hit.entity.get('random')}")
-
-print(search_latency_fmt.format(end_time - start_time))
-
-# -----------------------------------------------------------------------------
-# query based on scalar filtering(boolean, int, etc.)
-print(fmt.format("Start querying with `random > -14`"))
-
-start_time = time.time()
-result = hello_milvus.query(expr="random > -14", output_fields=["random", "embeddings"])
-end_time = time.time()
-
-print(f"query result:\n-{result[0]}")
-print(search_latency_fmt.format(end_time - start_time))
-
-# -----------------------------------------------------------------------------
-# hybrid search
-print(fmt.format("Start hybrid searching with `random > -12`"))
-
-start_time = time.time()
-result = hello_milvus.search(vectors_to_search, "embeddings", search_params, limit=3, expr="random > -12", output_fields=["random"])
-end_time = time.time()
-
-for hits in result:
-    for hit in hits:
-        print(f"hit: {hit}, random field: {hit.entity.get('random')}")
-
-print(search_latency_fmt.format(end_time - start_time))
-
-###############################################################################
-# 6. delete entities by PK
-# You can delete entities by their PK values using boolean expressions.
-ids = insert_result.primary_keys
-expr = f"pk in [{ids[0]}, {ids[1]}]"
-print(fmt.format(f"Start deleting with expr `{expr}`"))
-
-result = hello_milvus.query(expr=expr, output_fields=["random", "embeddings"])
-print(f"query before delete by expr=`{expr}` -> result: \n-{result[0]}\n-{result[1]}\n")
-
-hello_milvus.delete(expr)
-
-result = hello_milvus.query(expr=expr, output_fields=["random", "embeddings"])
-print(f"query after delete by expr=`{expr}` -> result: {result}\n")
+TIMEOUT = 120
 
 
-###############################################################################
-# 7. drop collection
-# Finally, drop the hello_milvus collection
-print(fmt.format("Drop collection `hello_milvus`"))
-utility.drop_collection("hello_milvus")
+def hello_milvus(host="127.0.0.1"):
+    import time
+    # create connection
+    connections.connect(host=host, port="19530")
+
+    print(f"\nList collections...")
+    print(list_collections())
+
+    # create collection
+    dim = 128
+    default_fields = [
+        FieldSchema(name="int64", dtype=DataType.INT64, is_primary=True),
+        FieldSchema(name="float", dtype=DataType.FLOAT),
+        FieldSchema(name="varchar", dtype=DataType.VARCHAR, max_length=65535),
+        FieldSchema(name="float_vector", dtype=DataType.FLOAT_VECTOR, dim=dim)
+    ]
+    default_schema = CollectionSchema(fields=default_fields, description="test collection")
+
+    print(f"\nCreate collection...")
+    collection = Collection(name="hello_milvus", schema=default_schema)
+
+    print(f"\nList collections...")
+    print(list_collections())
+
+    #  insert data
+    nb = 3000
+    vectors = [[random.random() for _ in range(dim)] for _ in range(nb)]
+    t0 = time.time()
+    collection.insert(
+        [
+            [i for i in range(nb)],
+            [np.float32(i) for i in range(nb)],
+            [str(i) for i in range(nb)],
+            vectors
+        ]
+    )
+    t1 = time.time()
+    print(f"\nInsert {nb} vectors cost {t1 - t0:.4f} seconds")
+
+    t0 = time.time()
+    print(f"\nGet collection entities...")
+    collection.flush()
+    print(collection.num_entities)
+    t1 = time.time()
+    print(f"\nGet collection entities cost {t1 - t0:.4f} seconds")
+
+    # create index and load table
+    default_index = {"index_type": "IVF_SQ8", "metric_type": "L2", "params": {"nlist": 64}}
+    print(f"\nCreate index...")
+    t0 = time.time()
+    collection.create_index(field_name="float_vector", index_params=default_index)
+    t1 = time.time()
+    print(f"\nCreate index cost {t1 - t0:.4f} seconds")
+    print("\nGet replicas number")
+    try:
+        replicas_info = collection.get_replicas()
+        replica_number = len(replicas_info.groups)
+        print(f"\nReplicas number is {replica_number}")
+    except Exception as e:
+        print(str(e))
+        replica_number = 1
+    print(f"\nload collection...")
+    t0 = time.time()
+    collection.load(replica_number=replica_number)
+    t1 = time.time()
+    print(f"\nload collection cost {t1 - t0:.4f} seconds")
+
+    # load and search
+    topK = 5
+    search_params = {"metric_type": "L2", "params": {"nprobe": 10}}
+    t0 = time.time()
+    print(f"\nSearch...")
+    # define output_fields of search result
+    res = collection.search(
+        vectors[-2:], "float_vector", search_params, topK,
+        "int64 > 100", output_fields=["int64", "float"], timeout=TIMEOUT
+    )
+    t1 = time.time()
+    print(f"search cost  {t1 - t0:.4f} seconds")
+    # show result
+    for hits in res:
+        for hit in hits:
+            # Get value of the random value field for search result
+            print(hit, hit.entity.get("float"))
+
+    # query
+    expr = "int64 in [2,4,6,8]"
+    output_fields = ["int64", "float"]
+    res = collection.query(expr, output_fields, timeout=TIMEOUT)
+    sorted_res = sorted(res, key=lambda k: k['int64'])
+    for r in sorted_res:
+        print(r)
+
+
+parser = argparse.ArgumentParser(description='host ip')
+parser.add_argument('--host', type=str, default='127.0.0.1', help='host ip')
+args = parser.parse_args()
+# add time stamp
+print(f"\nStart time: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}")
+hello_milvus(args.host)
